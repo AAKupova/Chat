77ee913067d7449ddaefa4c658e1de826bc04887
Kupova Anastatia <aa.kupova@yandex.ru>
2022-03-18 12:13:42 +0300

    refactor(chat): refactor component EditAvatar

--- chat/src/components/EditAvatar/index.jsx
+++ chat/src/components/EditAvatar/index.jsx
@@ -1,3 +1,10 @@
-<form>
-  <input class="edit-avatar" type="file" name="avatar" accept="image/*" value=''>
-</form> 
+
+import React from "react";
+
+import "./index.scss";
+
+export const EditAvatar = () => (
+  <form>
+   <input className="edit-avatar" type="file" name="avatar" accept="image/*" value='' />
+  </form> 
+)
